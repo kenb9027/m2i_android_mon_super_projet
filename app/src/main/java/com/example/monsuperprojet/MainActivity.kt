5b4939e79765a6398602f0c5f4e30f5d27a38c6a
kenb9027 <kenb9027@gmail.com>
2023-01-31 11:26:29 +0100

    initial commit

--- app/src/main/java/com/example/monsuperprojet/MainActivity.kt
+++ app/src/main/java/com/example/monsuperprojet/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.monsuperprojet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //btn Presentation
+        val buttonPresentation = findViewById<Button>(R.id.my_presentation_btn)
+        buttonPresentation.setOnClickListener {
+            val intentPresentation = Intent(this, PresentationActivity::class.java )
+            startActivity(intentPresentation)
+        }
+
+        //btn Music
+        val buttonMusic = findViewById<Button>(R.id.my_music_btn)
+        buttonMusic.setOnClickListener {
+            val intentMusic = Intent(this, MusicActivity::class.java )
+            startActivity(intentMusic)
+        }
     }
 }
---
+++ app/src/main/java/com/example/monsuperprojet/PresentationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.monsuperprojet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PresentationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_presentation)
+    }
+}
